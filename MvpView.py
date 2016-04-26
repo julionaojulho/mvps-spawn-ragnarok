21fcbb79d8fd38e16558de6e567c180c421735cd
julionaojulho <ferreiraalvesjulio@gmail.com>
2016-04-25 22:23:47 -0300

    Added files via upload

--- MvpView.py
+++ MvpView.py
@@ -1,22 +1,23 @@
-import string
 # https://docs.python.org/3.5/library/datetime.html ler depois
 
-class MvpView():
-    def __init__(self,name,time=0):
-        pass     # não sei direito o que fazer aqui
+# import string
+
+class MvpView:
+    def __init__(self,name):
+        self.mvp = name
 
     def add_entry(self):
         """
         Ask user for input and store its values
         """
-        mvp_name = input("Name: ")
+        mvp_name = self.mvp
         while mvp_name.lower() not in mvp_list:
-            print "Mvp not found, please, write the name again.\n"
+            print("Mvp not found, please, write the name again.\n")
             mvp_name = input("Name: ")
-        mvp_deathtime = input("Death time (hh:mm:ss): ")
-        mvp.seconds = int(mvp_deathtime[-2:])
-        mvp.minutes = int(mvp_deathtime[-5:-3])
-        mvp.hours = int(mvp_deathtime[:-6])
+        self.deathtime = input("Death time (hh:mm:ss): ")
+        self.mvp.seconds = int(self.deathtime[-2:])
+        self.mvp.minutes = int(self.deathtime[-5:-3])
+        self.mvp.hours = int(self.deathtime[:-6])
 
     def next_on_list(self,mvp_time = []):
         """
@@ -24,3 +25,5 @@ class MvpView():
         """
         self.queued = []
         return self.queued
+
+    a = MvpView()
